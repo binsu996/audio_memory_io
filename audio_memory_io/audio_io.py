14253b93307500caa41533ca55b054a54050c262
binsu996 <63957758+binsu996@users.noreply.github.com>
2024-09-10 04:01:11 +0000

    add main code

---
+++ audio_memory_io/audio_io.py
@@ -0,0 +1,37 @@
+import soundfile as sf
+import io
+import librosa
+import pathlib as path
+
+
+def load_bytes(bytes_data, **librosa_kwargs):
+    audio_in_memory = io.BytesIO(bytes_data)
+    return librosa.load(audio_in_memory, **librosa_kwargs)
+
+
+def load(filename_or_bytes, **librosa_kwargs):
+    if Path(filename_or_bytes).exists():
+        return librosa.load(filename_or_bytes, **librosa_kwargs)
+    else:
+        try:
+            return load_bytes(filename_or_bytes, **librosa_kwargs)
+        except:
+            raise RuntimeError(
+                "filename_or_bytes is neither an existing path nor a bytes object")
+
+
+def save(y, sr, filename=None, **soundfile_kwargs):
+
+    if len(y.shape) == 2:
+        y = y.T
+
+    if filename is None:
+        format = soundfile_kwargs.pop("format", "WAV")
+        buffer = io.BytesIO()
+        sf.write(buffer, y, 24000, format=format, **soundfile_kwargs)
+        buffer.seek(0)
+        binary_data = buffer.getvalue()
+        return binary_data
+    else:
+        sf.write(filename, y, sr, **soundfile_kwargs)
+        return None
